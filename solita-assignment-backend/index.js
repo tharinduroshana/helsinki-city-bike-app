b4afbcb510fc9f4df34624a35886ee017275774a
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-05-22 22:05:43 +0530

    Added new endpoints to retrieve list of trips and single trip entry

--- solita-assignment-backend/index.js
+++ solita-assignment-backend/index.js
@@ -5,6 +5,15 @@ app.get('/', (req, res) => {
     res.send('Test Endpoint!');
 });
 
+app.get('/trips', (req, res) => {
+    res.send('LIST_ALL');
+});
+
+app.get('/trips/:id', (req, res) => {
+    const { params: { id } } = req;
+    res.send('Requested trip: ' + id);
+})
+
 app.listen(3000, () => {
     console.log("Server listening on port: " + 3000);
 })
