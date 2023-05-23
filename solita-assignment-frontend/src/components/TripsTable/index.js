9cc49d020a2d24187a4cd6b5dd36ab698bb90880
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-05-23 22:32:42 +0530

    Added materialUI for react and added trips table

---
+++ solita-assignment-frontend/src/components/TripsTable/index.js
@@ -0,0 +1,132 @@
+import {useState, useEffect} from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+const columns = [
+    {id: 'id', label: 'ID', minWidth: 170},
+    {id: 'departure_date', label: 'Departure', minWidth: 100},
+    {
+        id: 'departure_station_name',
+        label: 'Departure Station',
+        minWidth: 170,
+        align: 'right',
+    },
+    {
+        id: 'covered_distance',
+        label: 'Distance (m)',
+        minWidth: 170,
+        align: 'right',
+    },
+    {
+        id: 'duration',
+        label: 'Duration (s)',
+        minWidth: 170,
+        align: 'right',
+    },
+];
+
+const TripsTable = () => {
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [trips, setTrips] = useState([]);
+
+    const fetchTrips = async () => {
+        try {
+            const response = await fetch('http://localhost:4000/trips');
+            const json = await response.json();
+            setTrips(json);
+        } catch (e) {
+            console.error("Error fetching trips.", e);
+        }
+    }
+
+    useEffect(() => {
+        fetchTrips();
+    });
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+    const renderTripsTable = () => {
+        return (
+            <Paper sx={{width: '100%', overflow: 'hidden'}}>
+                <TableContainer sx={{maxHeight: 440}}>
+                    <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                                {columns.map((column) => (
+                                    <TableCell
+                                        key={column.id}
+                                        align={column.align}
+                                        style={{minWidth: column.minWidth}}
+                                    >
+                                        {column.label}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {trips
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row) => {
+                                    return (
+                                        <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                            {columns.map((column) => {
+                                                const value = row[column.id];
+                                                return (
+                                                    <TableCell key={column.id} align={column.align}>
+                                                        {column.format && typeof value === 'number'
+                                                            ? column.format(value)
+                                                            : value}
+                                                    </TableCell>
+                                                );
+                                            })}
+                                        </TableRow>
+                                    );
+                                })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[10, 25, 100]}
+                    component="div"
+                    count={trips.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        );
+    }
+
+    const renderSpinner = () => {
+        return (
+            <Box sx={{display: 'flex'}}>
+                <CircularProgress/>
+            </Box>
+        );
+    }
+
+    return (
+        <>
+            {trips.length === 0 ? (renderSpinner()) : (renderTripsTable())}
+        </>
+    );
+}
+
+export default TripsTable;
