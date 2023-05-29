7a04cf3c3047552e66e1d74858760f7609b1b713
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-05-29 23:32:02 +0530

    Added stations list page

--- solita-assignment-frontend/src/App.js
+++ solita-assignment-frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import TripsTable from "./components/TripsTable";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TripScreen from "./components/TripScreen";
+import StationsTable from "./components/StationsTable";
 
 function App() {
   return (
     <Router>
         <Routes>
             <Route path="/" element={<TripsTable />} />
+            <Route path="/stations" element={<StationsTable />} />
             <Route path="/trip/:id" element={<TripScreen />} />
         </Routes>
     </Router>
---
+++ solita-assignment-frontend/src/components/StationsTable/index.js
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import TableComponent from "../Table";
+import {useNavigate} from "react-router-dom";
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const columns = [
+    {id: 'station_id', label: 'Station ID'},
+    {id: 'station_name', label: 'Station Name', align: 'right'},
+];
+
+const StationsTable = () => {
+    const [ stations, setStations ] = useState([]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const navigate = useNavigate();
+    const fetchStations = async () => {
+        try {
+            const response = await fetch('http://localhost:4000/stations');
+            const json = await response.json();
+            console.log(json)
+            setStations(json);
+        } catch (e) {
+            console.error("Error fetching trips.", e);
+        }
+    }
+
+    useEffect(() => {
+        fetchStations();
+    }, []);
+
+    const handleOnClick = () => {
+
+    }
+
+    const renderSpinner = () => {
+        return (
+            <Box sx={{display: 'flex'}}>
+                <CircularProgress/>
+            </Box>
+        );
+    }
+
+    const renderStationsTable = () => {
+        return (
+            <>
+                <TableComponent
+                    columns={columns}
+                    data={stations || []}
+                    onClickHandler={handleOnClick}
+                    rowsPerPage={rowsPerPage}
+                    rowPerPageHandler={() => {console.log("row clicked!")}}
+                    changePageHandler={() => {console.log("page clicked!")}}
+                    page={page}
+                />
+            </>
+        )
+    }
+
+    return (
+        <>
+            <div className="trips-screen">
+                <h1 className="trips-title">Stations</h1>
+                {stations.length === 0 ? (renderSpinner()) : (renderStationsTable())}
+            </div>
+        </>
+    );
+}
+
+export default StationsTable;
