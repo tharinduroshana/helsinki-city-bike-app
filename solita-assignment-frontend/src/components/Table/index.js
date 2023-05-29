73e89b4db23a069b7174826dede8647c5372bf63
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-05-29 23:08:33 +0530

    Refactored and Created reusable table component

---
+++ solita-assignment-frontend/src/components/Table/index.js
@@ -0,0 +1,65 @@
+import Paper from "@mui/material/Paper";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import TablePagination from "@mui/material/TablePagination";
+import {Table} from "@mui/material";
+
+const TableComponent = ({ columns, data = [], rowsPerPage, page, onClickHandler, changePageHandler, rowPerPageHandler }) => {
+  return (
+      <div className="trip-table">
+          <Paper sx={{width: '100%', overflow: 'hidden'}}>
+              <TableContainer sx={{maxHeight: 500}}>
+                  <Table stickyHeader aria-label="sticky table">
+                      <TableHead>
+                          <TableRow>
+                              {columns.map((column) => (
+                                  <TableCell
+                                      key={column.id}
+                                      align={column.align}
+                                      style={{minWidth: column.minWidth}}
+                                  >
+                                      {column.label}
+                                  </TableCell>
+                              ))}
+                          </TableRow>
+                      </TableHead>
+                      <TableBody>
+                          {data
+                              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                              .map((row) => {
+                                  return (
+                                      <TableRow onClick={() => onClickHandler(row.id)} hover role="checkbox" tabIndex={-1} key={row.id}>
+                                          {columns.map((column) => {
+                                              const value = row[column.id];
+                                              return (
+                                                  <TableCell key={column.id} align={column.align}>
+                                                      {column.format && typeof value === 'number'
+                                                          ? column.format(value)
+                                                          : value}
+                                                  </TableCell>
+                                              );
+                                          })}
+                                      </TableRow>
+                                  );
+                              })}
+                      </TableBody>
+                  </Table>
+              </TableContainer>
+              <TablePagination
+                  rowsPerPageOptions={[10, 25, 100]}
+                  component="div"
+                  count={data.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={changePageHandler}
+                  onRowsPerPageChange={rowPerPageHandler}
+              />
+          </Paper>
+      </div>
+  );
+}
+
+export default TableComponent;
--- solita-assignment-frontend/src/components/TripsTable/index.js
+++ solita-assignment-frontend/src/components/TripsTable/index.js
@@ -1,16 +1,9 @@
 import {useState, useEffect} from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import { useNavigate } from 'react-router-dom';
 import "./TripsTable.css";
+import TableComponent from "../Table";
 
 const columns = [
     {id: 'id', label: 'ID'},
@@ -73,57 +66,17 @@ const TripsTable = () => {
 
     const renderTripsTable = () => {
         return (
-            <div className="trip-table">
-                <Paper sx={{width: '100%', overflow: 'hidden'}}>
-                    <TableContainer sx={{maxHeight: 500}}>
-                        <Table stickyHeader aria-label="sticky table">
-                            <TableHead>
-                                <TableRow>
-                                    {columns.map((column) => (
-                                        <TableCell
-                                            key={column.id}
-                                            align={column.align}
-                                            style={{minWidth: column.minWidth}}
-                                        >
-                                            {column.label}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {trips
-                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                    .map((row) => {
-                                        return (
-                                            <TableRow onClick={() => handleOnClick(row.id)} hover role="checkbox" tabIndex={-1} key={row.id}>
-                                                {columns.map((column) => {
-                                                    const value = row[column.id];
-                                                    return (
-                                                        <TableCell key={column.id} align={column.align}>
-                                                            {column.format && typeof value === 'number'
-                                                                ? column.format(value)
-                                                                : value}
-                                                        </TableCell>
-                                                    );
-                                                })}
-                                            </TableRow>
-                                        );
-                                    })}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                    <TablePagination
-                        rowsPerPageOptions={[10, 25, 100]}
-                        component="div"
-                        count={trips.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onPageChange={handleChangePage}
-                        onRowsPerPageChange={handleChangeRowsPerPage}
-                    />
-                </Paper>
-            </div>
-        );
+            <>
+                <TableComponent
+                    page={page}
+                    changePageHandler={handleChangePage}
+                    rowPerPageHandler={handleChangeRowsPerPage}
+                    onClickHandler={handleOnClick}
+                    rowsPerPage={rowsPerPage}
+                    columns={columns}
+                    data={trips || []} />
+            </>
+        )
     }
 
     const renderSpinner = () => {
