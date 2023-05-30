b2937299d00d225cc119854f90f2d69c7bfac895
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-05-30 23:01:54 +0530

    Added component tests

--- solita-assignment-frontend/src/App.test.js
+++ solita-assignment-frontend/src/App.test.js
@@ -1,8 +1,46 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
+import {BrowserRouter} from "react-router-dom";
+import TripsTable from "./screens/TripsTable";
+import StationsTable from "./screens/StationsTable";
+import StationDetails from "./screens/StationDetails";
+import TripScreen from "./screens/TripScreen";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("TripsTable", () => {
+  it("renders correctly", async () => {
+    render(
+        <BrowserRouter>
+          <TripsTable />
+        </BrowserRouter>
+    );
+  });
+});
+
+describe("StationsTable", () => {
+  it("renders correctly", async () => {
+    render(
+        <BrowserRouter>
+          <StationsTable />
+        </BrowserRouter>
+    );
+  });
+});
+
+describe("StationDetails", () => {
+  it("renders correctly", async () => {
+    render(
+        <BrowserRouter>
+          <StationDetails />
+        </BrowserRouter>
+    );
+  });
+});
+
+describe("TripScreen", () => {
+  it("renders correctly", async () => {
+    render(
+        <BrowserRouter>
+          <TripScreen />
+        </BrowserRouter>
+    );
+  });
 });
