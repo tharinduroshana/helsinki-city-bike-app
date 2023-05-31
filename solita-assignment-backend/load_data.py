a41870d8275a7d075b239c9909bca2db250a476d
Tharindu <ranaweerat@gmail.com>
2023-05-31 17:16:34 +0530

    Added comments for data loading files

--- solita-assignment-backend/load_data.py
+++ solita-assignment-backend/load_data.py
@@ -2,26 +2,36 @@ import pandas as pd
 import mysql.connector
 import os
 
+# This python file loads trips detail CSV into trips table in helsinki_city_bike_db database.
+
+# Configs needed to establish database connection. Replace root/root with relevant username/password
+db_username = 'root'
+db_password = 'root'
+
 config = {
-    'user': 'root',
-    'password': 'root@123',
+    'user': db_username,
+    'password': db_password,
     'host': 'localhost'
 }
+# Name of the database
 database_name = "helsinki_city_bike_db"
+# Name of the trips table
 table_name = "trips"
 
 cnx = mysql.connector.connect(**config)
 
 cursor = cnx.cursor()
 
+# Creates a new database if not already exists
 cursor.execute("CREATE DATABASE IF NOT EXISTS `{}`".format(database_name))
 
 cursor.close()
 cnx.close()
 
+# Configs needed to establish database connection.
 config = {
-    'user': 'root',
-    'password': 'root@123',
+    'user': db_username,
+    'password': db_password,
     'host': 'localhost',
     'database': database_name
 }
@@ -30,18 +40,23 @@ cnx = mysql.connector.connect(**config)
 
 cursor = cnx.cursor()
 
+# Creates new trips table if not exists
 query = "CREATE TABLE IF NOT EXISTS {} (id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, departure_date VARCHAR(50) NOT NULL, return_date VARCHAR(50) NOT NULL, departure_station_id VARCHAR(50) NOT NULL, departure_station_name VARCHAR(50) NOT NULL, return_station_id VARCHAR(50) NOT NULL, return_station_name VARCHAR(50) NOT NULL, covered_distance INT(50) NOT NULL, duration INT(50) NOT NULL)".format(table_name)
 
 cursor.execute(query)
 
+# Finds the files with .csv file extension in the ./data directory
 file_list = os.listdir('./data')
 csv_files = [file for file in file_list if file.endswith('.csv')]
 
 csv_files = ['./data/' + file for file in csv_files]
 
+# Loops through the .csv files
 for csv_file in csv_files:
     df = pd.read_csv(csv_file)
+    # Replaces nulls with zeros
     df = df.where(pd.notnull(df), 0)
+    # filters trips which are not shorter than 10 meters and 10 seconds
     df = df[(df['Duration (sec.)'] > 10) & (df['Covered distance (m)'] > 10)]
     data = [tuple(row) for row in df.values]
 
@@ -51,6 +66,7 @@ for csv_file in csv_files:
 
     columns = 'departure_date, return_date, departure_station_id, departure_station_name, return_station_id, return_station_name, covered_distance, duration'
 
+    # Prepares the queries to enter records
     insert_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
 
     cursor.executemany(insert_query, data)
--- solita-assignment-backend/load_station_data.py
+++ solita-assignment-backend/load_station_data.py
@@ -2,26 +2,36 @@ import pandas as pd
 import mysql.connector
 import os
 
+# This python file loads stations detail CSV into stations table in helsinki_city_bike_db database.
+
+# Configs needed to establish database connection. Replace root/root with relevant username/password
+db_username = 'root'
+db_password = 'root'
+
 config = {
-    'user': 'root',
-    'password': 'root@123',
+    'user': db_username,
+    'password': db_password,
     'host': 'localhost'
 }
+# Name of the database
 database_name = "helsinki_city_bike_db"
+# Name of the stations table
 table_name = "stations"
 
 cnx = mysql.connector.connect(**config)
 
 cursor = cnx.cursor()
 
+# Creates a new database if not already exists
 cursor.execute("CREATE DATABASE IF NOT EXISTS `{}`".format(database_name))
 
 cursor.close()
 cnx.close()
 
+# Configs needed to establish database connection. Replace root/root with relevant username/password
 config = {
-    'user': 'root',
-    'password': 'root@123',
+    'user': db_username,
+    'password': db_password,
     'host': 'localhost',
     'database': database_name
 }
@@ -30,17 +40,21 @@ cnx = mysql.connector.connect(**config)
 
 cursor = cnx.cursor()
 
+# Creates new stations table if not exists
 query = "CREATE TABLE IF NOT EXISTS {} (fid INT(11) NOT NULL PRIMARY KEY, id INT(11) NOT NULL, nimi VARCHAR(50) NOT NULL, namn VARCHAR(50) NOT NULL, name VARCHAR(50) NOT NULL, osoite VARCHAR(50) NOT NULL, adress VARCHAR(50) NOT NULL, kaupunki VARCHAR(50) NOT NULL, stad VARCHAR(50) NOT NULL, operaattor VARCHAR(50) NOT NULL, kapasiteet VARCHAR(50) NOT NULL, x VARCHAR(50) NOT NULL, y VARCHAR(50) NOT NULL)".format(table_name)
 
 cursor.execute(query)
 
+# Finds the files with .csv file extension in the ./station_data directory
 file_list = os.listdir('./station_data')
 csv_files = [file for file in file_list if file.endswith('.csv')]
 
 csv_files = ['./station_data/' + file for file in csv_files]
 
+# Loops through the .csv files
 for csv_file in csv_files:
     df = pd.read_csv(csv_file)
+    # Replaces nulls with zeros
     df = df.where(pd.notnull(df), 0)
     data = [tuple(row) for row in df.values]
 
@@ -50,6 +64,7 @@ for csv_file in csv_files:
 
     columns = 'fid, id, nimi, namn, name, osoite, adress, kaupunki, stad, operaattor, kapasiteet, x, y'
 
+    # Prepares the queries to enter records
     insert_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
 
     cursor.executemany(insert_query, data)
